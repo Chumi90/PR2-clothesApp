091150ead6023347398eeafed13e5b7aac59d5d8
Chumi90 <abel_chumillas@hotmail.com>
2025-06-08 15:44:54 +0200

    Third commit loging Admin Check

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,39 +3,41 @@ const express = require('express');
 const session = require('express-session');
 const app = express.Router();
 const middlewares = require('../middleware/authMiddleware.js');
-    app.get('/products/login', (req, res) => {
-      
-      const mensajeError = req.query.error
-        ? (req.query.error === '1' ? 'Palabra incorrecta, inténtalo de nuevo.' : 'No estás logado.')
-        : '';
-      if (req.session.palabraSecreta) {
-        return res.redirect('/dashboard');
-      }
-      res.send(`
-        <html>
-          <body>
-            <h1>Página de Inicio</h1>
-            <p>${mensajeError}</p>
-            <form method="post" action="/products/login/user">
-              <label for="palabra">Introduce la palabra:</label>
-              <input type="text" name="palabra" required>
-              <button type="submit">Enviar</button>
-            </form>
-          </body>
-        </html>
-      `);
+
+app.get('/products/login', (req, res) => {
+  const mensajeError = req.query.error
+  ? (req.query.error === '1' ? 'Palabra incorrecta, inténtalo de nuevo.' : 'No estás logado.'): '';
+  if (req.session.palabraSecreta) {
+    return res.redirect('/dashboard');
+  }
+  res.send(`
+      <html>
+        <body>
+          <h1>Página de Inicio</h1>
+          <p>${mensajeError}</p>
+          <form method="post" action="/products/login/user">
+            <label for="palabra">Introduce la palabra:</label>
+            <input type="text" name="palabra" required>
+            <button type="submit">Entrar</button>
+          </form>
+          <form method="post" action="/">
+            <button type="submit">Salir</button>
+          </form>
+        </body>
+      </html>
+    `);
   });
 
 
 
   app.get('/products/login/user', middlewares.verificarSesionMiddleware, (req, res) => {
-  res.send(`
-    <h1>Ruta del Perfil (Sesión activa)</h1>
-    <form method="post" action="/logout">
-      <button type="submit">Log Out</button>
-    </form>
-  `);
-  console.log("pasa")
+    res.send(`
+      <h1>Ruta del Perfil (Sesión activa)</h1>
+      <form method="post" action="/logout">
+        <button type="submit">Log Out</button>
+      </form>
+    `);
+    console.log("pasa")
   });
 
   app.post('/products/login/user', middlewares.validarPalabraMiddleware, (req, res) => {
@@ -44,12 +46,8 @@ const middlewares = require('../middleware/authMiddleware.js');
 
 
 
-  
-  
-
  // Utilizar para el boton de salida del dashboard
   app.post('/', (req, res) => {
-    console.log('funciona')
     req.session.destroy((err) => {
       if (err) {
         console.error('Error al cerrar sesión:', err);
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,20 +5,19 @@ const router = express.Router();
 const Product = require('../models/Product.js');
 const ProductController = require('../controllers/productController.js');
 
-let ProductCategory='';
-let rutas='';
+const middlewares = require('../middleware/authMiddleware.js');//Protección de las rutas
 
 //DashBoard
-router.get('/dashboard/new', ProductController.newProduct);//Formulario
-router.post('/dashboard', ProductController.createProduct);//Post para crear en Mongo DB
-router.get('/dashboard/created', ProductController.showProductCreated);//Producto creado
-router.get('/dashboard', ProductController.products); //Muestra todos los productos
-router.get('/dashboard/:_id', ProductController.product); //devuelve el detalle del producto
+router.get('/dashboard/new', ProductController.newProduct,middlewares.verificarSesionMiddleware);//Formulario
+router.post('/dashboard', ProductController.createProduct, middlewares.verificarSesionMiddleware);//Post para crear en Mongo DB
+router.get('/dashboard/created', middlewares.verificarSesionMiddleware, ProductController.showProductCreated);//Producto creado
+router.get('/dashboard', ProductController.products, middlewares.verificarSesionMiddleware); //Muestra todos los productos
+router.get('/dashboard/:_id', ProductController.product, middlewares.verificarSesionMiddleware); //devuelve el detalle del producto
 
-router.get('/dashboard/:_id/edit', ProductController.productModify);//Muestra el formulario
-router.post('/dashboard/:_id/edit', ProductController.productChanged); //Envía el formulario
-router.get('/dashboard/:_id/delete', ProductController.deleteProduct);//Muestra el formulario
-router.get('/dashboard/:category/category', ProductController.productsCategoryDashboard);//Muestra por categoria
+router.get('/dashboard/:_id/edit', ProductController.productModify, middlewares.verificarSesionMiddleware);//Muestra el formulario
+router.post('/dashboard/:_id/edit', ProductController.productChanged, middlewares.verificarSesionMiddleware); //Envía el formulario
+router.get('/dashboard/:_id/delete', ProductController.deleteProduct, middlewares.verificarSesionMiddleware);//Muestra el formulario
+router.get('/dashboard/:category/category', ProductController.productsCategoryDashboard, middlewares.verificarSesionMiddleware);//Muestra por categoria
 
 //Pagina principal de productos para clientes
 router.get('/products', ProductController.productsClients); //Muestra todos los productos
